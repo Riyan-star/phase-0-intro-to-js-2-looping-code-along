3c9fa278f10fc98f11a14ac216e563ff78b78ebc
+ <rgigs123@gmail.com>
2025-07-16 23:28:36 +0300

    dindex.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+// Function: writeCards
+function writeCards(names, eventName) {
+  const thankYouMessages = [];
+
+  for (let i = 0; i < names.length; i++) {
+    thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+  }
+
+  return thankYouMessages;
+}
+
+// Function: countDown
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
